b5c1d7210a29ef54f6ec294c3d58125c368d85ab
Daniel Pedersen <mail@danielpedersen.me>
2015-12-22 10:31:32 +0100

    fix permissions on executable

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require("gulp")
 var babel = require("gulp-babel")
+var chmod = require("gulp-chmod")
 
 gulp.task("default", function () {
   gulp.src("hickup.js")
     .pipe(babel())
+    .pipe(chmod(755))
     .pipe(gulp.dest("dist/"))
 })
